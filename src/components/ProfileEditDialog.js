d7ab8587a45e42bb98468701c1842f7ade4e0939
anutka-mak <annmakiichuk@gmail.com>
2024-01-28 02:27:59 +0200

    updated the form for editing, added new fields

--- src/components/ProfileEditDialog.js
+++ src/components/ProfileEditDialog.js
@@ -5,7 +5,8 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import TextInputField from './TextInputField';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import TextInputField from "./TextInputField";
 
 export default function ProfileEditDialog() {
     const [open, setOpen] = useState(false);
@@ -16,6 +17,10 @@ export default function ProfileEditDialog() {
         oldPassword: '',
         newPassword: '',
         confirmPassword: '',
+        fullName: '',
+        dateOfBirth: '',
+        about: '',
+        iconFile: null,
     });
     const [changePassword, setChangePassword] = useState(false);
 
@@ -24,19 +29,35 @@ export default function ProfileEditDialog() {
     };
 
     const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
+        const { name, value, type, files } = event.target;
+
+        if (type === 'file') {
+            setFormData({
+                ...formData,
+                [name]: files[0],
+            });
+        } else {
+            setFormData({
+                ...formData,
+                [name]: value,
+            });
+        }
     };
 
-    const handleSubmit = async () => { 
-
+    const handleSave = async () => {
+        //
+        setTimeout(() => {
+            handleClose();
+        }, 1000);
     };
+
+    const handleSavePassword = async () => {
+        setChangePassword(false);
+    };
+
     return (
         <div>
-            <Button onClick={() => setOpen(true)} sx={{color:"white", marginRight:"40px"}}>Редагувати</Button>
+            <Button onClick={() => setOpen(true)} sx={{ color: "white", marginRight: "40px" }}>Редагувати</Button>
 
             <Dialog open={open} onClose={handleClose}>
                 <DialogTitle>Редагування профілю</DialogTitle>
@@ -44,57 +65,105 @@ export default function ProfileEditDialog() {
                     <DialogContentText>
                         Змініть дані профілю:
                     </DialogContentText>
-                    <TextInputField
-                        label="Старий логін"
-                        name="oldLogin"
-                        type="text"
-                        value={formData.oldLogin}
-                        onChange={handleChange}
-                    />
-                    <TextInputField
-                        label="Новий логін"
-                        name="newLogin"
-                        type="text"
-                        value={formData.newLogin}
-                        onChange={handleChange}
-                    />
-                    {changePassword && (
+                    <div style={{ marginBottom: '16px', marginTop: '16px', }}>
+                        <label htmlFor="fullName">Прізвище та ім'я</label>
+                        <TextInputField
+                            id="fullName"
+                            name="fullName"
+                            type="text"
+                            value={formData.fullName}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div style={{ marginBottom: '16px' }}>
+                        <label htmlFor="dateOfBirth" style={{ marginRight: 6 }}>Дата народження (DD/MM/YYYY)</label>
+                        <input
+                            id="dateOfBirth"
+                            name="dateOfBirth"
+                            type="text"
+                            value={formData.dateOfBirth}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div style={{ marginBottom: '16px' }}>
+                        <label htmlFor="iconFile" style={{ marginRight: 6 }}>Додайте іконку</label>
+                        <input
+                            type="file"
+                            accept="image/*"
+                            id="iconFile"
+                            name="iconFile"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div style={{ marginBottom: '16px' }}>
+                        <label htmlFor="about">Опис профілю</label>
+                        <TextareaAutosize
+                            id="about"
+                            minRows={4}
+                            placeholder="Опис профілю"
+                            name="about"
+                            value={formData.about}
+                            onChange={handleChange}
+                            style={{ width: '100%', padding: '8px', resize: 'vertical', marginTop: 6 }}
+                        />
+                    </div>
+                    {changePassword ? (
                         <>
-                            <TextInputField
-                                label="Старий пароль"
-                                name="oldPassword"
-                                type="password"
-                                value={formData.oldPassword}
-                                onChange={handleChange}
-                            />
-                            <TextInputField
-                                label="Новий пароль"
-                                name="newPassword"
-                                type="password"
-                                value={formData.newPassword}
-                                onChange={handleChange}
-                            />
-                            <TextInputField
-                                label="Підтвердіть свій новий пароль"
-                                name="confirmPassword"
-                                type="password"
-                                value={formData.confirmPassword}
-                                onChange={handleChange}
-                            />
+                            <div style={{ marginBottom: '16px' }}>
+                                <label htmlFor="oldPassword">Старий пароль</label>
+                                <TextInputField
+                                    id="oldPassword"
+                                    name="oldPassword"
+                                    type="password"
+                                    value={formData.oldPassword}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div style={{ marginBottom: '16px' }}>
+                                <label htmlFor="newPassword">Новий пароль</label>
+                                <TextInputField
+                                    id="newPassword"
+                                    name="newPassword"
+                                    type="password"
+                                    value={formData.newPassword}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div style={{ marginBottom: '16px' }}>
+                                <label htmlFor="confirmPassword">Підтвердіть свій новий пароль</label>
+                                <TextInputField
+                                    id="confirmPassword"
+                                    name="confirmPassword"
+                                    type="password"
+                                    value={formData.confirmPassword}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <DialogActions>
+                                <Button onClick={handleSavePassword} color="primary">
+                                    Зберегти пароль
+                                </Button>
+                                <Button onClick={() => setChangePassword(false)} color="primary">
+                                    Скасувати
+                                </Button>
+                            </DialogActions>
                         </>
+                    ) : (
+                        <Button onClick={() => setChangePassword(true)}>
+                            Змінити пароль
+                        </Button>
                     )}
-                    <Button onClick={() => setChangePassword(!changePassword)}>
-                        Змінити пароль
-                    </Button>
                 </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Скасувати
-                    </Button>
-                    <Button onClick={handleSubmit} color="primary">
-                        Зберегти
-                    </Button>
-                </DialogActions>
+                {!changePassword && (
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">
+                            Скасувати
+                        </Button>
+                        <Button onClick={handleSave} color="primary">
+                            Зберегти
+                        </Button>
+                    </DialogActions>
+                )}
             </Dialog>
         </div>
     );
