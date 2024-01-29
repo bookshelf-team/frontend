748c9d856885af622d2868c95594ef0cbd179be1
anutka-mak <annmakiichuk@gmail.com>
2024-01-29 14:38:18 +0200

    added logic for genre selection and changed the list of genres

--- src/components/BookGenresAppBar.js
+++ src/components/BookGenresAppBar.js
@@ -8,26 +8,54 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import BookIcon from '@mui/icons-material/Book';
 import List from "@mui/material/List";
+import {useDispatch} from "react-redux";
+import {searchBooksByGenre} from "../redux/getBooks/getBooksService";
+import {setSearchResults} from "../redux/getBooks/getBooksActions";
+import {useNavigate} from "react-router-dom";
 
-const genres = [
-    'Фантастика',
-    'Детектив',
-    'Роман',
-    'Поезія',
-    'Історія',
-    'Наукова література',
-    'Фантастика',
-    'Детектив',
-    'Роман',
-    'Поезія',
-    'Історія',
-    'Наукова література',
-];
+const genreMappings = {
+    'Фентезі': 'Fantasy',
+    'Пригоди': 'Adventure',
+    'Романтика': 'Romance',
+    'Сучасна література': 'Contemporary',
+    'Дистопія': 'Dystopian',
+    'Містика': 'Mystery',
+    'Жахи': 'Horror',
+    'Трилер': 'Thriller',
+    'Паранормальне': 'Paranormal',
+    'Історична художня література': 'Historical_Fiction',
+    'Наукова фантастика': 'Science_Fiction',
+    'Дитяча література': 'Children',
+    'Мемуари': 'Memoir',
+    'Кулінарна книга': 'Cookbook',
+    'Мистецтво': 'Art',
+    'Самодопомога': 'Self_Help',
+    'Особистісний розвиток': 'Personal_Development',
+    'Мотиваційна література': 'Motivational',
+    'Здоровя': 'Health',
+    'Історія': 'History',
+    'Подорожі': 'Travel',
+    'Путівник': 'Guide',
+    'Відносини': 'Relationships',
+    'Гумор': 'Humor',
+};
 
-export default function BookGenresAppBar({ isOpen, toggleDrawer, onGenreSelect }) {
-    const handleGenreClick = (genre) => {
-        onGenreSelect(genre);
+export default function BookGenresAppBar({ isOpen, toggleDrawer }) {
+    let navigate = useNavigate();
+    const genres = Object.keys(genreMappings);
+    const dispatch = useDispatch();
+
+    const handleGenreClick = async (genre) => {
         toggleDrawer(false);
+        try {
+            const englishGenre = genreMappings[genre];
+            const books = await searchBooksByGenre(englishGenre);
+            console.log(englishGenre);
+            dispatch(setSearchResults(books));
+            navigate('/catalogpage');
+        } catch (error) {
+
+        }
     };
 
     const columns = genres.reduce((acc, genre, index) => {
@@ -49,7 +77,7 @@ export default function BookGenresAppBar({ isOpen, toggleDrawer, onGenreSelect }
     }, []);
 
     return (
-        <Drawer anchor='top' open={isOpen} onClose={() => toggleDrawer(false)}>
+        <Drawer anchor="top" open={isOpen} onClose={() => toggleDrawer(false)}>
             <Box
                 sx={{ width: 'auto' }}
                 role="presentation"
