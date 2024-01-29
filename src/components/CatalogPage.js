6dcf8fdacecfe1dcae0afe0b7d720cebbe13efb2
anutka-mak <annmakiichuk@gmail.com>
2024-01-29 14:39:05 +0200

    added display logic depending on the request

--- src/components/CatalogPage.js
+++ src/components/CatalogPage.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import BooksGrid from './BooksGrid';
 import AppBar from './AppBar';
-import BookGenresAppBar from './BookGenresAppBar';
+import {Typography} from "@mui/material";
 
 const CatalogPage = () => {
-    const books = useSelector((state) => state.books);
+    const books = useSelector((state) => state.bookCatalogSearch.searchResultsCatalog);
 
     return (
         <div>
             <AppBar />
-            <BookGenresAppBar />
-            <h1>Catalog</h1>
-            <BooksGrid books={books} />
+            {books.length > 0 ? (
+                <BooksGrid books={books} />
+            ) : (
+                <Typography variant="h6" color="textSecondary" style={{ textAlign: 'center', marginTop: '20px', color: 'white', fontSize: 23 }}>
+                    За вашим запитом нічого не знайдено
+                </Typography>
+            )}
         </div>
     );
 };
