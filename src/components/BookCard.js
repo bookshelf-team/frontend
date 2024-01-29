1f3af1389ba903f4389a70b421927cbfbe55dc8c
anutka-mak <annmakiichuk@gmail.com>
2024-01-29 14:37:11 +0200

    added a reflection for another picture and increased the size

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardActionArea, CardMedia } from '@mui/material';
+import { Card, CardActionArea, CardMedia} from '@mui/material';
 
 const BookCard = ({ book, isCatalog }) => {
     const handleClick = () => {
@@ -13,8 +13,8 @@ const BookCard = ({ book, isCatalog }) => {
         <Card sx={{ maxWidth: 170, maxHeight: 240 }} onClick={handleClick}>
             <CardActionArea>
                 <CardMedia
-                    sx={{ width: 170, height: 240, m: 'auto', display: 'block' }}
-                    image={book.image}
+                    sx={{ width: 170, height: 265, m: 'auto', display: 'block' }}
+                    image={book.coverImageUrl || book.image}
                 />
             </CardActionArea>
         </Card>
