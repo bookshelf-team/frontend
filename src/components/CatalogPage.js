32404a34bc57a5c9c4364d554c2948b3ba7a6597
anutka-mak <annmakiichuk@gmail.com>
2024-01-29 14:50:50 +0200

    added a button to return to the main page

--- src/components/CatalogPage.js
+++ src/components/CatalogPage.js
@@ -3,13 +3,27 @@ import { useSelector } from 'react-redux';
 import BooksGrid from './BooksGrid';
 import AppBar from './AppBar';
 import {Typography} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import {useNavigate} from "react-router-dom";
 
 const CatalogPage = () => {
     const books = useSelector((state) => state.bookCatalogSearch.searchResultsCatalog);
+    const navigate = useNavigate();
 
+    const handleBackToHome = () => {
+        navigate('/');
+    };
     return (
         <div>
             <AppBar />
+            <IconButton
+                onClick={handleBackToHome}
+                aria-label="back to home"
+                style={{ color: 'white', marginLeft: '10px', marginTop: '30px' }}
+            >
+                <ArrowBackIosNewIcon />
+            </IconButton>
             {books.length > 0 ? (
                 <BooksGrid books={books} />
             ) : (
