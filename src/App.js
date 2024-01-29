2aea6b64f1e7665c6724dc932c866199956b98c5
anutka-mak <annmakiichuk@gmail.com>
2024-01-29 14:34:43 +0200

    added output to the console for verification

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ProfileEditDialog from "./components/ProfileEditDialog";
 import SearchResultsPage from "./components/SearchResultsPage";
 import BookPage from "./pages/bookPage";
 import CatalogPage from "./components/CatalogPage";
+import {bookAPI} from "./api";
 
 function App() {
 
@@ -27,5 +28,27 @@ function App() {
         </Router>
     );
 }
+
+
+bookAPI.getAllBooks().then((books) => {
+    console.log("All Books:", books);
+});
+
+bookAPI.getBookById(1).then((book) => {
+    console.log("Book by ID:", book);
+});
+
+
+bookAPI.getBooksByTitle("Дюна").then((books) => {
+    console.log("Books by Title:", books);
+});
+
+bookAPI.getBooksByAuthor("J.K. Rowling").then((books) => {
+    console.log("Books by Author:", books);
+});
+
+bookAPI.getBooksByGenre("Science_Fiction").then((books) => {
+    console.log("Books by Genre:", books);
+});
 
 export default App;
