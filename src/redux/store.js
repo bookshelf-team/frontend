18d9e7192bde623cbbf6df67e5d76b191a8dea69
anutka-mak <annmakiichuk@gmail.com>
2024-02-01 23:47:30 +0200

    add a state for your profile and book page

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,16 @@ import authReducer from "./auth-reducer";
 import bookSearchReducer from "./bookSearch/bookSearchReducer";
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {thunk} from "redux-thunk";
+import profileReducer from "./profile/profileReducer";
 import bookSearchCatalogReducer from "./getBooks/getBooksReducer";
 
 const reducers = combineReducers({
     auth: authReducer,
+    profile: profileReducer,
     bookSearch: bookSearchReducer,
     bookCatalogSearch: bookSearchCatalogReducer,
-
+    bookPage: bookSearchCatalogReducer,
 });
 
 const store = createStore(reducers, applyMiddleware(thunk));
-
 export default store;
