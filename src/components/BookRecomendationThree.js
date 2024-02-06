63f3c3369ad201cf10a841257532a472ccf9ab3b
anutka-mak <annmakiichuk@gmail.com>
2024-02-06 14:26:51 +0200

    added a component to display 3 books recommended for reading

---
+++ src/components/BookRecomendationThree.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { Box, Typography, Grid, CardMedia } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSearchResults } from '../redux/getBooks/getBooksActions';
+import { getAllBooks } from '../redux/getBooks/getBooksService';
+
+export default function BookRecommendationsThree() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const books = await dispatch(getAllBooks());
+                dispatch(setSearchResults(books));
+            } catch (error) {
+                console.error("Error while fetching books:", error);
+            }
+        };
+
+        fetchBooks();
+    }, [dispatch]);
+
+    const recommendedBooks = useSelector((state) => state.bookCatalogSearch.searchResultsCatalog);
+
+    const displayedBooks = Array.isArray(recommendedBooks) && recommendedBooks.length >= 8
+        ? recommendedBooks.slice(3, 6)
+        : [];
+
+    return (
+        <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 1, py: 1, marginLeft: 2 }}>
+            <Grid container spacing={0}>
+                {displayedBooks.map((book, index) => (
+                    <Grid item xs={4} sm={4} md={2} lg={1.5} key={index}>
+                        <Link to={`/book/${book.isbn}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 150, height: 250, m: 'auto', display: 'block' }}
+                                image={book.coverImageUrl || 'fallback-image-url.jpg'}
+                                alt={`Обкладинка книги ${book.title}`}
+                            />
+                            <Typography variant="body1" component="div" color="white" sx={{ textAlign: 'center', marginTop: 1 }}>
+                                {book.title}
+                            </Typography>
+                            <Typography variant="body2" color="white" sx={{ textAlign: 'center' }}>
+                                {book.author}
+                            </Typography>
+                        </Link>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    );
+}
